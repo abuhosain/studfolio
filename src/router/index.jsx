3c530ba144416583414c590422b506687784e8ea
helal3419 <mdhelalhossen4103@gmail.com>
2025-10-30 19:48:14 +0600

    created development branch

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -6,6 +6,7 @@ import Students from "../components/Students";
 import Contact from "../components/Contact";
 import AbuHosainProfile from "../pages/student/Hosain-715534";
 import HelalProfile from "../pages/student/Helal-715524";
+import RakibulProfile from "../pages/student/Rakibul-715523";
 
 const router = createBrowserRouter([
   {
@@ -31,6 +32,10 @@ const router = createBrowserRouter([
       {
         path: "student/715534",
         element: <AbuHosainProfile />,
+      },
+      {
+        path: "student/715523",
+        element: <RakibulProfile />,
       },
       {
         path: "student/715524",
