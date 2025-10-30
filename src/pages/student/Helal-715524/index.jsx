8cb168b1a06a72eaf3ec26b564779621d90782e0
helal3419 <mdhelalhossen4103@gmail.com>
2025-10-30 19:52:49 +0600

    updated my page

--- src/pages/student/Helal-715524/index.jsx
+++ src/pages/student/Helal-715524/index.jsx
@@ -1,7 +1,8 @@
 const HelalProfile = () => {
     return (
         <div>
-            <h1 className="text-center text-4xl">Welcome to Studfolio</h1>
+            <h1 className="text-center text-4xl">Welcome to my portfolio</h1>
+            <h2 className="text-center text-4xl">I am Helal Hosen</h2>
         </div>
     )
 };
