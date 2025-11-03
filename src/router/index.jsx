75f71ee413a70b158abd86b3e5ca0ccdbf6504b0
SKMPranto <prantokumar3463@gmail.com>
2025-11-03 22:45:33 +0600

    4 students file created

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -6,6 +6,10 @@ import Students from "../components/Students";
 import Contact from "../components/Contact";
 import AbuHosainProfile from "../pages/student/Hosain-715534";
 import PrantoPortfolio from "../pages/student/Pranto-715499";
+import PolashPortfolio from "../pages/student/Polash-515401";
+import ALAminPortfolio from "../pages/student/Alamin-616383";
+import BarshaPortfolio from "../pages/student/Barsha-616384";
+import NayemPortfolio from "../pages/student/Nayem-702938";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +31,22 @@ const router = createBrowserRouter([
       {
         path: "contact",
         element: <Contact />,
+      },
+      {
+        path: "student/515401",
+        element: <PolashPortfolio />,
+      },
+      {
+        path: "student/616383",
+        element: <ALAminPortfolio />,
+      },
+      {
+        path: "student/616384",
+        element: <BarshaPortfolio />,
+      },
+      {
+        path: "student/702938",
+        element: <NayemPortfolio />,
       },
       {
         path: "student/715534",
